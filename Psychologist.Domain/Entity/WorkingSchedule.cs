155b09f0829df929e385c7763963cd82ae1cd56e
Achi00 <toolvlg@gmail.com>
2026-08-03 20:12:34 +0400

    added calendar entity

---
+++ Psychologist.Domain/Entity/WorkingSchedule.cs
@@ -0,0 +1,13 @@
+﻿namespace Psychologist.Domain.Entity
+{
+    // defines calendar data and aveilable work times for booking
+    public sealed class WorkingSchedule
+    {
+        public Guid Id { get; set; }
+        public Guid PsychologistId { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public bool IsUnavailable { get; set; }
+    }
+}
