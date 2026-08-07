5e5c14646a5edb526ce45683b8c6e0c033183c57
Achi00 <toolvlg@gmail.com>
2026-08-07 19:26:32 +0400

    created email smpt intefrace and contract

---
+++ PsychologistSystem.Application/Interfaces/Services/Email/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Mail;
+
+namespace PsychologistSystem.Application.Interfaces.Services.Email
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailMessage message, CancellationToken ct = default);
+    }
+}
---
+++ PsychologistSystem.Application/Services/Auth/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using PsychologistSystem.Application.DTOs.Auth;
+using PsychologistSystem.Application.Interfaces.JWT;
+using PsychologistSystem.Application.Interfaces.Services.Auth;
+using PsychologistSystem.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace PsychologistSystem.Application.Services.Auth
+{
+    public sealed class AuthService : IAuthService
+    {
+        private readonly IIdentityService _identityService;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        //TODO: add email service for notifications
+
+        public AuthService(IIdentityService identityService, IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _identityService = identityService;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+        public async Task<EmailConfirmationResult> RegisterAsync(RegisterUserRequest request)
+        {
+            //var (status, userId, errors) = await _identityService.CreateUserAsync(request.Email, request.UserName, request.Password);
+
+            //if (status == IdentityResultStatus.Failed)
+            //{
+            //    throw new Exception();
+            //}
+
+            //var token = await _identityService.GenerateEmailConfirmationTokenAsync(userId);
+            throw new NotImplementedException();
+            
+        }
+
+        public Task ConfirmEmailAsync(string userId, string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
