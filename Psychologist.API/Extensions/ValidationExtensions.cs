3312f89a3b1ef379ed61b12a2775b2e0ceb5480e
Achi00 <toolvlg@gmail.com>
2026-08-09 14:38:22 +0400

    created infrastructure di registration class

--- Psychologist.API/Extensions/ValidationExtensions.cs
+++ Psychologist.API/Extensions/ValidationExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using PsychologistSystem.Infrastructure.Validators;
+using PsychologistSystem.Persistance.Identity;
 
 namespace PsychologistSystem.API.Extensions
 {
@@ -7,7 +10,11 @@ namespace PsychologistSystem.API.Extensions
     {
         public static IServiceCollection AddValidation(this IServiceCollection services)
         {
-            services.AddFluentValidationAutoValidation();
+    //        services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+    //                .AddPasswordValidator<PreventCurrentPasswordValidator>()
+    //.AddEntityFrameworkStores<AppDbCont>()
+    //.AddDefaultTokenProviders();
+    //        services.AddFluentValidationAutoValidation();
 
             return services;
         }
---
+++ Psychologist.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PsychologistSystem.Application.Contracts.Auth;
+using PsychologistSystem.Application.Contracts.Email;
+using PsychologistSystem.Application.Interfaces.JWT;
+using PsychologistSystem.Application.Interfaces.Services.Auth;
+using PsychologistSystem.Application.Interfaces.Services.Email;
+using PsychologistSystem.Infrastructure.Auth;
+using PsychologistSystem.Infrastructure.Email;
+using PsychologistSystem.Infrastructure.Validators;
+using PsychologistSystem.Persistance.Context;
+using PsychologistSystem.Persistance.Identity;
+
+namespace PsychologistSystem.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options => { })
+           .AddPasswordValidator<PreventCurrentPasswordValidator>()
+           .AddEntityFrameworkStores < ApplicationDbContext>()
+           .AddDefaultTokenProviders();
+
+            services.Configure<JwtSettings>(config.GetSection("Jwt"));
+            services.Configure<EmailSettings>(config.GetSection("Email"));
+
+            services.AddScoped<IIdentityService, IdentityService>();
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddScoped<IEmailService, MailKitEmailService>();
+
+            return services;
+        }
+    }
+}
