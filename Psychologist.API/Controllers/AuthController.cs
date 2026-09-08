61662dcab6a983b509ad8a1f3c6fe9dd43df8538
Achi00 <toolvlg@gmail.com>
2026-09-08 20:10:39 +0400

    added login endpoint

---
+++ Psychologist.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PsychologistSystem.Application.DTOs.Auth;
+using PsychologistSystem.Application.Interfaces.Services.Auth;
+
+namespace PsychologistSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request, CancellationToken ct = default)
+        {
+            var result = await _authService.LoginAsync(request, ct);
+
+            Response.Cookies.Append("refreshToken", result.RefreshToken!, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTimeOffset.UtcNow.AddDays(7),
+                Path = "/api/auth/refresh"
+            });
+
+            return Ok(new { accessToken = result.AccessToken, expiresAt = result.ExpiresAt });
+        }
+    }
+}
