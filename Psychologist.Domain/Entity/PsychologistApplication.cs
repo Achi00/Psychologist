2af6761c113576ee2960e5f73cad1b1a6722487c
Achi00 <toolvlg@gmail.com>
2026-09-12 22:07:45 +0400

    starting psychologist auth flow

---
+++ Psychologist.Domain/Entity/PsychologistApplication.cs
@@ -0,0 +1,19 @@
+﻿using PsychologistSystem.Domain.Enums;
+
+namespace PsychologistSystem.Domain.Entity
+{
+    public sealed class PsychologistApplication
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Description { get; set; } = default!;
+        // TODO: can be collection of different categories
+        public Guid CategoryId { get; set; }
+        public PsychologistApplicationStatus Status { get; set; } = PsychologistApplicationStatus.Pending;
+        public DateTimeOffset SubmittedAt { get; set; }
+        public DateTimeOffset? ReviewedAt { get; set; }
+        // admin only
+        public Guid? ReviewedByUserId { get; set; }
+        public string? RejectionReason { get; set; }
+    }
+}
---
+++ PsychologistSystem.Application/DTOs/Auth/PsychologistApplicationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace PsychologistSystem.Application.DTOs.Auth
+{
+    public sealed record PsychologistApplicationRequest(
+        string Email,
+        string Firstname,
+        string Lastname,
+        string Password,
+        string Description,
+        string LicenseNumber
+    );
+}
