27aa98974aea0d1fda8ccf850ea9d2f8ce88951a
Achi00 <toolvlg@gmail.com>
2026-09-12 23:25:10 +0400

    register missing resvices

--- Psychologist.Infrastructure/DependencyInjection.cs
+++ Psychologist.Infrastructure/DependencyInjection.cs
@@ -3,12 +3,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PsychologistSystem.Application.Contracts.Auth;
 using PsychologistSystem.Application.Contracts.Email;
+using PsychologistSystem.Application.Interfaces;
 using PsychologistSystem.Application.Interfaces.JWT;
+using PsychologistSystem.Application.Interfaces.Repositories;
 using PsychologistSystem.Application.Interfaces.Repositories.Psychologists;
 using PsychologistSystem.Application.Interfaces.Services.Auth;
 using PsychologistSystem.Application.Interfaces.Services.Email;
 using PsychologistSystem.Infrastructure.Auth;
 using PsychologistSystem.Infrastructure.Email;
+using PsychologistSystem.Infrastructure.Repositories;
 using PsychologistSystem.Infrastructure.Repositories.Psychologists;
 using PsychologistSystem.Infrastructure.Validators;
 using PsychologistSystem.Persistance.Context;
@@ -34,6 +37,8 @@ namespace PsychologistSystem.Infrastructure
 
             services.AddScoped<IPsychologistRepository, PsychologistRepository>();
             services.AddScoped<IPsychologistApplicationRepository, PsychologistApplicationRepository>();
+            services.AddScoped<IRefreshTokenRepository,  RefreshTokenRepository>();
+            services.AddScoped<IUnitOfWork,  UnitOfWork>();
 
             return services;
         }
