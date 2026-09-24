83302af17a9aa72518616e46b59bceb4db199670
Achi00 <toolvlg@gmail.com>
2026-09-24 21:20:59 +0400

    add minute check on schedule validator

--- PsychologistSystem.Application/Validators/Booking/CreateWorkingScheduleRequestValidator.cs
+++ PsychologistSystem.Application/Validators/Booking/CreateWorkingScheduleRequestValidator.cs
@@ -7,9 +7,18 @@ namespace PsychologistSystem.Application.Validators.Booking
     {
         public CreateWorkingScheduleRequestValidator()
         {
-            RuleFor(x => x.StartTime).Must(t => t.Minute == 0).WithMessage("Start time must be on the hour.");
-            RuleFor(x => x.EndTime).Must(t => t.Minute == 0).WithMessage("End time must be on the hour.");
-            RuleFor(x => x).Must(x => x.EndTime > x.StartTime).WithMessage("End time must be after start time.");
+            RuleFor(x => x.DayOfWeek)
+                .IsInEnum();
+
+            RuleFor(x => x.StartTime)
+                .Must(t => t.Minute == 0 && t.Second == 0)
+                .WithMessage("Start time must be on the hour.");
+
+            RuleFor(x => x.EndTime)
+                .Must(t => t.Minute == 0 && t.Second == 0)
+                .WithMessage("End time must be on the hour.")
+                .GreaterThan(x => x.StartTime)
+                .WithMessage("End time must be after start time.");
         }
     }
 }
